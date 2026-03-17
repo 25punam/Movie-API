6753f2e7a9ae33027d33c77dbf2588a401103b2f
Base25Punam <25punamgode@gmail.com>
2026-03-17 22:09:44 +0530

    Added project with CI/CD pipeline

--- movies/models.py
+++ movies/models.py
@@ -7,34 +7,7 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-
-# class Movie(models.Model):
-#     id = models.BigAutoField(primary_key=True)
-#     movie_id = models.PositiveBigIntegerField(unique=True, null=False, db_index=True)
-
-#     title = models.CharField(max_length=255,db_index=True)
-#     original_title = models.CharField(max_length=255)
-#     original_language = models.CharField(max_length=10)
-
-#     overview = models.TextField(blank=True)
-
-#     adult = models.BooleanField(default=False)
-#     video = models.BooleanField(default=False)
-
-#     popularity = models.FloatField(default=0.0)
-#     vote_average = models.FloatField(default=0.0)
-#     vote_count = models.PositiveIntegerField(default=0)
-
-#     release_date = models.DateField(null=True, blank=True)
-
-#     poster_path = models.ImageField(upload_to="movies/posters/",null=True,blank=True)
-#     backdrop_path = models.ImageField(upload_to="movies/backdrops/",null=True,blank=True)
-
-#     genres = models.ManyToManyField(Genre,related_name="movies",blank=True)
-
-#     def __str__(self):
-#         return self.title
-    
+  
 
 class Movie(models.Model):
     id = models.BigAutoField(primary_key=True)
